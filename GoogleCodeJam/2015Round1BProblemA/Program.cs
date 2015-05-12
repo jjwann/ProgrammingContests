39e62899fcf1b3b5665125fa2593d0ec8423a11c
JWann <jimmy(at)warhammer.com>
2015-05-12 00:39:55 -0400

    Added Google Code Jam

---
+++ GoogleCodeJam/2015Round1BProblemA/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main()
+    {
+        using (FileStream iStream = File.OpenRead(@"C:\Users\James\Downloads\input.in"))
+        {
+            using (FileStream oStream = File.OpenWrite(@"C:\Users\James\Documents\Visual Studio 2012\Projects\TopCoder\TopCoder\output.out"))
+            {
+                using (StreamReader sr = new StreamReader(iStream))
+                {
+                    int t = int.Parse(sr.ReadLine());
+                    string o = "Case #{0}: ";
+
+                    using (StreamWriter sw = new StreamWriter(oStream))
+                    {
+                        int e;
+                        long n, r;
+
+                        int[] d = new int[14];
+                        d[0] = 10;
+
+                        for (int i = 1; i < d.Length; i++)
+                        {
+                            d[i] = d[i - 1] + Calculate((long)Math.Pow(10, i + 1) - 1, i + 1) + 1;
+                        }
+                        
+                        for (int i = 0; i < t; i++)
+                        {
+                            n = long.Parse(sr.ReadLine());
+
+                            if (n < 10)
+                            {
+                                r = n;
+                            }
+                            else
+                            {
+                                e = 2;
+
+                                while (n > Math.Pow(10, e))
+                                    e++;
+
+                                r = d[e - 2];
+
+                                if (n != (long)Math.Pow(10, e - 1))
+                                    r += n % 10 == 0 ? Calculate(n - 1, e) + 1 : Calculate(n, e);
+                            }
+
+                            sw.WriteLine(string.Format(o, i + 1) + r);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static int Calculate(long value, int places)
+    {
+        int r = 0;
+        int h, a;
+        
+        h = places >> 1;
+        a = 0;
+
+        for (int j = 0; j < h; j++)
+        {
+            a += (int)(value % 10) * (int)Math.Pow(10, j);
+            value /= 10;
+        }
+
+        r += a;
+
+        if (places % 2 > 0)
+        {
+            r += (int)(value % 10) * (int)Math.Pow(10, h);
+            value /= 10;
+        }
+
+        if (value != (int)Math.Pow(10, h - 1))
+        {
+            a = 0;
+            for (int j = 0; j < h; j++)
+            {
+                a = a * 10 + (int)(value % 10);
+                value /= 10;
+            }
+
+            r += a;
+        }
+
+        return r;
+    }
+}
---
+++ GoogleCodeJam/2015Round1CProblemB/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main()
+    {
+        using (FileStream iStream = File.OpenRead(@"C:\Users\James\Downloads\input.in"))
+        {
+            using (FileStream oStream = File.OpenWrite(@"C:\Users\James\Documents\Visual Studio 2012\Projects\TopCoder\TopCoder\output.out"))
+            {
+                using (StreamReader sr = new StreamReader(iStream))
+                {
+                    int t = int.Parse(sr.ReadLine());
+                    string o = "Case #{0}: ";
+
+                    using (StreamWriter sw = new StreamWriter(oStream))
+                    {
+                        int[] s;
+                        double r, e;
+                        int l;
+                        string a;
+                        for (int i = 0; i < t; i++)
+                        {
+                            s = sr.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+                            a = sr.ReadLine();
+                            target = sr.ReadLine();
+                            
+                            prob = a.GroupBy(x => x).Select(x => new { c = x.Key, p = (double)x.Count() / a.Length }).ToDictionary(x => x.c, x => x.p);
+
+                            //int subIndex;
+                            subIndex = 0;
+                            subIndices = new int[target.Length];
+                            int index = 1;
+
+                            while (index < target.Length)
+                            {
+                                if (target[0] == target[index])
+                                {
+                                    //subIndex = 0;
+                                    for (int k = index; k < target.Length; k++)
+                                    {
+                                        if (k + 1 == target.Length && target[k - index] == target[k])
+                                        {
+                                            subIndex = index;
+                                            index = target.Length;
+                                        }
+                                        else if (target[k - index] != target[k])
+                                        {
+                                            subIndices[k] = k - index;
+                                            index = k;
+                                            break;
+                                        }
+                                    }
+                                }
+                                else
+                                    index++;
+                            }
+
+                            expect = new double[target.Length][];
+
+                            for (int k = 0; k < expect.Length; k++)
+                            {
+                                expect[k] = Enumerable.Repeat(-1, s[2] + 1).Select(x => (double)x).ToArray();
+                            }
+
+                            r = 0d;
+                            e = Expected(0, s[2]);
+                            
+                            if (e > 0)
+                            {
+                                l = 0;
+                                while (s[2] - l >= target.Length)
+                                {
+                                    r++;
+                                    l += subIndex == 0 ? target.Length : subIndex;
+                                }
+                            }
+
+                            r -= e;
+                            sw.WriteLine(string.Format(o, i + 1) + r);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static int subIndex;
+    private static int[] subIndices;
+
+    private static string target;
+    private static Dictionary<char, double> prob;
+
+    private static double[][] expect;
+
+    public static double Expected(int index, int length)
+    {
+        if (index == target.Length)
+            return 0;
+
+        if (expect[index][length] == -1)
+        {
+
+            if (target.Length - index > length)
+                expect[index][length] = 0;
+            else
+            {
+                double p, p0, p1;
+                prob.TryGetValue(target[index], out p);
+                prob.TryGetValue(target[0], out p0);
+                prob.TryGetValue(target[subIndices[index]], out p1);
+
+                if (index + 1 == target.Length)
+                {
+                    if (subIndex > 0)
+                    {
+                        if (target[index] == target[0])
+                            expect[index][length] = p * (1 + Expected(target.Length - subIndex, length - 1)) + (1 - p) * Expected(0, length - 1);
+                        else
+                            expect[index][length] = p * (1 + Expected(target.Length - subIndex, length - 1)) + p0 * Expected(1, length - 1) + (1 - p - p0) * Expected(0, length - 1);
+                    }
+                    else if (subIndices[index] > 0)
+                        expect[index][length] = p * (1 + Expected(0, length - 1)) + p0 * Expected(1, length - 1) + p1 * Expected(subIndices[index] + 1, length - 1) + (1 - p - p0 - p1) * Expected(0, length - 1);
+                    else if (target[index] == target[0])
+                        expect[index][length] = p * (1 + Expected(0, length - 1)) + (1 - p) * Expected(0, length - 1);
+                    else
+                        expect[index][length] = p * (1 + Expected(0, length - 1)) + p0 * Expected(1, length - 1) + (1 - p - p0) * Expected(0, length - 1);
+                }
+                else if (subIndices[index] > 0)
+                    expect[index][length] = p * Expected(index + 1, length - 1) + p0 * Expected(1, length - 1) + p1 * Expected(subIndices[index] + 1, length - 1) + (1 - p - p0 - p1) * Expected(0, length - 1);
+                else if (target[index] == target[0])
+                    expect[index][length] = p * Expected(index + 1, length - 1) + (1 - p) * Expected(0, length - 1);
+                else
+                    expect[index][length] = p * Expected(index + 1, length - 1) + p0 * Expected(1, length - 1) + (1 - p - p0) * Expected(0, length - 1);
+            }
+        }
+         
+        return expect[index][length];
+    }
+}
